3d2783f604b125f923d5f878564920d62f7d30af
John Adenusi <johnadenusi@johns-mac.home>
2019-02-05 00:15:18 +0000

    JS

--- main.tf
+++ main.tf
@@ -7,34 +7,36 @@
 #}
 
 variable "AWS_REGION" {
-    type = "string"
+  type = "string"
 }
 
 variable "region-ami-map" {
-    type = "map"
-    default = {
-        "us-east-1" = "ami-cd0f5cb6"
-        "us-east-2" = "ami-10547475"
-        "us-west-1" = "ami-09d2fb69"
-        "us-west-2" = "ami-6e1a0117"
-        "ca-central-1" = "ami-9818a7fc"
-        "eu-central-1" = "ami-1e339e71"
-        "eu-west-1" = "ami-785db401"
-        "eu-west-2" = "ami-996372fd"
-        "ap-southeast-1" = "ami-6f198a0c"
-        "ap-southeast-2" = "ami-e2021d81"
-        "ap-northeast-1" = "ami-ea4eae8c"
-    }
+  type = "map"
+
+  default = {
+    "us-east-1"      = "ami-cd0f5cb6"
+    "us-east-2"      = "ami-10547475"
+    "us-west-1"      = "ami-09d2fb69"
+    "us-west-2"      = "ami-6e1a0117"
+    "ca-central-1"   = "ami-9818a7fc"
+    "eu-central-1"   = "ami-1e339e71"
+    "eu-west-1"      = "ami-785db401"
+    "eu-west-2"      = "ami-996372fd"
+    "ap-southeast-1" = "ami-6f198a0c"
+    "ap-southeast-2" = "ami-e2021d81"
+    "ap-northeast-1" = "ami-ea4eae8c"
+  }
 }
 
 provider "aws" {
-  region     = "${var.AWS_REGION}"
-  version    = "1.54"
-#  assume_role {
-#    role_arn     = "arn:aws:iam::${var.DEPLOY_INTO_ACCOUNT_ID}:role/TerraformRole"
-#    session_name = "Terraform"
-#    external_id  = "${var.ASSUME_ROLE_EXTERNAL_ID}"
-#  }
+  region  = "${var.AWS_REGION}"
+  version = "1.54"
+
+  #  assume_role {
+  #    role_arn     = "arn:aws:iam::${var.DEPLOY_INTO_ACCOUNT_ID}:role/TerraformRole"
+  #    session_name = "Terraform"
+  #    external_id  = "${var.ASSUME_ROLE_EXTERNAL_ID}"
+  #  }
 }
 
 provider "template" {
@@ -51,7 +53,6 @@ terraform {
 }
 
 resource "aws_instance" "ec2instance" {
-  ami = "${lookup(var.region-ami-map, var.AWS_REGION)}"
+  ami           = "${lookup(var.region-ami-map, var.AWS_REGION)}"
   instance_type = "t2.micro"
 }
-
